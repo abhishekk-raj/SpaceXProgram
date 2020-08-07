1ea7f9dd2baebcfa8c05e780db4f3a46ccf177e5
Abhishek Raj <abhishek@betatestsolutions.com>
2020-08-07 18:38:18 +0530

    Create basic required components

---
+++ src/app/components/program-filter/program-filter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sxp-program-filter',
+  templateUrl: './program-filter.component.html',
+  styleUrls: ['./program-filter.component.scss']
+})
+export class ProgramFilterComponent {
+
+}
---
+++ src/app/components/program-list/program-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sxp-program-list',
+  templateUrl: './program-list.component.html',
+  styleUrls: ['./program-list.component.scss']
+})
+export class ProgramListComponent {
+
+}
